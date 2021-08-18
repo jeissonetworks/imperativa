61ab630e0d6129fca6f1c0f43c90995eace4f0ca
jeissonetworks <jeissonetworks@gmail.com>
2021-08-17 20:01:42 -0500

    variables js

---
+++ Clase4/index.js
@@ -0,0 +1,47 @@
+//Viaje a la fiambreria
+let temperatura = 28
+let llueve = true
+let pisoAscensor = 5
+let horaActual = 10.30
+const horaApertura = 6.00
+const horaCierra = 20.00
+let cantidadQueso = 50
+let deuda = 500
+console.log("\n","Viaje a la fiambreria")
+console.log(temperatura, llueve, pisoAscensor, horaActual, horaActual, horaApertura, horaCierra, cantidadQueso, deuda)
+
+//Ir al trabajo
+
+const horaDespertar = 7.30
+let tomarCafe = 8.00
+let lavarDientes = true
+let horaDucha = 8.30
+let aguaFria = false
+let vestirse = true
+let horaSalida = 9.00
+let cogerBus = "Ruta24"
+let horaLlegada = 10.00
+console.log("\n","Ir al trabajo")
+console.log(horaDespertar, tomarCafe, lavarDientes, horaDucha, aguaFria, vestirse, horaSalida, cogerBus, horaLlegada)
+
+//Sacar a pasear al perro
+let checkBolsas = true;
+let nombrePerro = "Zeus";
+let llamoPerro = true;
+let colocoCorrea = true;
+let tiempoPaseo = 30; //minutos
+let juguetes = "huesito";
+let vuelvoACasa = true;
+console.log("\n","Sacar a pasear al perro")
+console.log(checkBolsas, nombrePerro,  llamoPerro, colocoCorrea, tiempoPaseo, juguetes, vuelvoACasa) ;
+
+//Desafío extra
+let cajonPantalones = "Remeras";
+let cajonRemeras = "Pantalones";
+let cajonVacio;
+
+cajonVacio = cajonPantalones; //llevo remeras al cajón vacío
+cajonPantalones = cajonRemeras; // cambio pantalones al cajón de los pantalones
+cajonRemeras = cajonVacio //Paso remeras a cajón remeras
+console.log("\n","Desafío extra",)
+console.log("cajonPantalones:",  cajonPantalones, "\n"+ "cajonRemeras:", cajonRemeras);
