9e75da4ceb3fcec87a6594fdfe2575c624f5ede7
jeissonetworks <jeissonetworks@gmail.com>
2021-09-09 17:11:32 -0500

    Objetos JS

--- Clase13/app.js
+++ Clase13/app.js
@@ -1,83 +1,120 @@
-//la lista de clientes.
 let arrayCuentas = [
-    {
-      nroCuenta: 5486273622,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 27771,
-      titularCuenta: "Abigael Natte",
-    },
-    {
-      nroCuenta: 1183971869,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 8675,
-      titularCuenta: "Ramon Connell",
-    },
-    {
-      nroCuenta: 9582019689,
-      tipoDeCuenta: "Caja de Ahorro",
-      saldoEnPesos: 27363,
-      titularCuenta: "Jarret Lafuente",
-    },
-    {
-      nroCuenta: 1761924656,
-      tipoDeCuenta: "Cuenta Corriente",
-      saldoEnPesos: 32415,
-      titularCuenta: "Ansel Ardley",
-    },
-    {
-      nroCuenta: 7401971607,
-      tipoDeCuenta: "Cuenta Unica",
-      saldoEnPesos: 18789,
-      titularCuenta: "Jacki Shurmer",
-    },
-  ];
-  
-  // Nos solicitan también crear un objeto literal llamado “banco”, el cual tendrá una propiedad llamada “clientes”, que estará compuesta de la lista de objetos obtenidos en el punto anterior.
-  
-  const banco = {
-    clientes: arrayCuentas,
-    //Al objeto “banco”, crearle un método llamado consultarCliente, el cual recibirá un nombre (titular) por parámetro, deberá buscarlo en la lista de cuentas y retornar el objeto cliente que corresponda con ese nombre ingresado.
-    
-    consultarCliente: function (titular) {
-      let mensajeError =
-        "No se encontraron coincidencias, por favor verifique los datos ingresados";
-      let clienteEncontrado;
-      for (let i = 0; i < this.clientes.length; i++) {
-        let cliente = this.clientes[i];
-        if (titular === cliente.titularCuenta) {
-          clienteEncontrado = cliente;
-        }
+  {
+    nroCuenta: 5486273622,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 27771,
+    titularCuenta: "Abigael Natte",
+  },
+  {
+    nroCuenta: 1183971869,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 8675,
+    titularCuenta: "Ramon Connell",
+  },
+  {
+    nroCuenta: 9582019689,
+    tipoDeCuenta: "Caja de Ahorro",
+    saldoEnPesos: 27363,
+    titularCuenta: "Jarret Lafuente",
+  },
+  {
+    nroCuenta: 1761924656,
+    tipoDeCuenta: "Cuenta Corriente",
+    saldoEnPesos: 32415,
+    titularCuenta: "Ansel Ardley",
+  },
+  {
+    nroCuenta: 7401971607,
+    tipoDeCuenta: "Cuenta Unica",
+    saldoEnPesos: 18789,
+    titularCuenta: "Jacki Shurmer",
+  },
+];
+
+const banco = {
+  clientes: arrayCuentas,
+  // Al objeto “banco”, crearle un método llamado consultarCliente, el cual recibirá un nombre (titular) por parámetro, deberá buscarlo en la lista de cuentas y retornar el objeto cliente que corresponda con ese nombre ingresado.
+  consultarCliente: function (titular) {
+    for (let i = 0; i < this.clientes.length; i++) {
+      if (titular === this.clientes[i].titularCuenta) {
+        return this.clientes[i];
       }
-      return clienteEncontrado ? clienteEncontrado : mensajeError;
-    },
-  
-    //Punto 4
-    deposito: function (titular, cantDinero){
-      let cliente = this.consultarCliente(titular)
-  
-      cliente.saldoEnPesos = cliente.saldoEnPesos + cantDinero
-      console.log("Depósito realizado, su nuevo saldo es: " + cliente.saldoEnPesos)
-    },
-  
-    // Punto 5
-    extraccion: function (titular, montoExtraer){
-      let cliente = this.consultarCliente(titular)
-  
-      cliente.saldoEnPesos = cliente.saldoEnPesos - montoExtraer
-  
-      if (cliente.saldoEnPesos < 0) {
-        console.log("Fondos insuficientes")
-      } else {
-        console.log("Extracción realizada correctamente, su nuevo saldo es: " + cliente.saldoEnPesos)
-      } 
-    },
-  };
-  
-  
-  let clienteEncontrado = banco.consultarCliente("Ansel Ardley");
-  let clienteEncontrado2 = banco.consultarCliente("leandro");
-  console.log(clienteEncontrado);
-  console.log(clienteEncontrado2); 
-  
-  banco.deposito("Ansel Ardley", 1000)
-  banco.extraccion("Ansel Ardley", 40000)
+    }
+  },
+  deposito: function (titular, cantidadADepositar) {
+    let totalADepositar =
+      banco.consultarCliente(titular).saldoEnPesos + cantidadADepositar;
+    return `Depósito realizado, su nuevo saldo es: ${totalADepositar}`;
+  },
+  extraccion: function (titular, cantidadAExtraer) {
+    let saldo = banco.consultarCliente(titular).saldoEnPesos - cantidadAExtraer;
+    if (saldo > 0) {
+      return `Extracción realizada correctamente, su nuevo saldo es: ${saldo}`;
+    } else {
+      return `Fondos insuficientes`;
+    }
+  },
+};
+console.log("------- Metodo ConsultarCliente -------");
+console.log(banco.consultarCliente("Jarret Lafuente"));
+console.log(banco.consultarCliente("Abigael"));
+console.log("------- Metodo Deposito -------");
+console.log(banco.deposito("Abigael Natte", 1000));
+console.log(banco.deposito("Abigael", 1000));
+console.log("------- Metodo Extraccion -------");
+console.log(banco.extraccion("Abigael Natte", 1000));
+console.log(banco.extraccion("Abigael Natte", 30000));
+console.log(banco.extraccion("Abigael ", 30000));
+
+/////////////// EXTRA BONUS /////////////////
+// Crear una función llamada propiedadUnica, que reciba un arreglo de objetos como parámetro y un string. Esta deberá retornar un nuevo arreglo de objetos, teniendo como parámetro la propiedad que fue pasada como string.
+// Ejemplo:
+// propiedadUnica(array, “edad”) debe retornar [ { edad: 27 }, { edad: 49 } ]
+// propiedadUnica(array, “nombre”) debe retornar [ { nombre: “Lean”}, { nombre: “Eze” } ]
+
+let array = [
+  { nombre: "Lean", edad: 27 },
+  { nombre: "Eze", edad: 49 },
+];
+function propiedadUnica(arr, str) {
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let obj = {
+      [str]: arr[i][str],
+    };
+    res.push(obj);
+  }
+  return res;
+}
+// let objetoCambiado = propiedadUnica(array, "edad");
+// console.log(objetoCambiado);
+
+////////////////////////////////////////
+// Crear el objeto “alumno”, el cual va a consistir en las siguientes propiedades básicas:
+// Nombre
+// Número de legajo
+// Lista de notas
+// Nos gustaría calcular el promedio del alumno y si el mismo está aprobado, basado en una nota de aprobación que le va a ser dada. Para este ejercicio, vamos a dejar que pienses todos los métodos y propiedades que puedan hacer falta para que el programa funcione correctamente de la manera solicitada.
+
+const alumno = {
+  nombre: "Leandro",
+  legajo: 123456,
+  listadoNotas: [7, 2, 3, 5, 10],
+  promedio: function () {
+    let suma = 0;
+    for (let i = 0; i < this.listadoNotas.length; i++) {
+      suma += this.listadoNotas[i];
+    }
+    return suma / this.listadoNotas.length;
+  },
+  estaAprobado: function (notaMinima) {
+    let promedio = this.promedio();
+    return `El alumno ${this.nombre} esta ${
+      promedio > notaMinima ? "aprobado" : "desaprobado"
+    }. Su nota promedio es: ${promedio}`;
+  },
+};
+
+// let alumnoAprobado = alumno.estaAprobado();
+// console.log(alumnoAprobado);
